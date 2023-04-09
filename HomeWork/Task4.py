dc89fdd09871e2d65bd50c2ea53eacab4dc56628
Artem <lebro53@mail.ru>
2023-04-09 13:18:13 +0300

    Add task6 and completed task2,4

--- HomeWork/Task4.py
+++ HomeWork/Task4.py
@@ -5,4 +5,19 @@
 # *Пример:*
 # 6 -> 1  4  1
 # 24 -> 4  16  4
-#     60 -> 10  40  10
+# 60 -> 10  40  10
+# a + b + (a + b) * 2 = 60
+# 3 * a + 3 * b = 60
+# a + b = 20
+
+
+sum_ships = int(input('Enter number of ships: '))
+
+if (sum_ships % 3 == 0):
+    boys_ships = sum_ships // 3
+    kate_ships = boys_ships * 2
+    print(f'Ships Kate: {kate_ships}, Petr: {boys_ships//2} and Sergey: {boys_ships//2}')
+else:
+    print('Please enter another number')
+
+
