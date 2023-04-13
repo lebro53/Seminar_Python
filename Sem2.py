4785bfcb09e047294fc76a02ea2b4925a8a1319a
Artem <lebro53@mail.ru>
2023-04-13 14:51:56 +0300

    Added solution task1 sem2

--- Sem2.py
+++ Sem2.py
@@ -1,7 +1,31 @@
-number = int(input('Enter number: '))
+# number = int(input('Enter number: '))
+# i = 1
+# result = 1
+# while (i <= number):
+#     result *= i
+#     i+=1
+# print(result)
+
+# Задача №11. Решение в группах
+# Дано натуральное число A > 1. Определите, каким по
+# счету числом Фибоначчи оно является, то есть
+# выведите такое число n, что φ(n)=A. Если А не
+# является числом Фибоначчи, выведите число -1.
+# Input: 5
+# Output: 6
+# 0 1 1 2 3 5 8 13 21 34 55 89
+
+fib_number = int(input('Enter number: '))
+fib_1 = 0
+fib_2 = 1
 i = 1
-result = 1
-while (i <= number):
-    result *= i
-    i+=1
-print(result)
+while (i <= fib_number):
+    fib_sum = fib_1 + fib_2
+    fib_1 = fib_2
+    fib_2 = fib_sum
+    i += 1
+    if fib_sum == fib_number:
+        print(i+1)
+        exit(0)
+print('This is number not found')
+
