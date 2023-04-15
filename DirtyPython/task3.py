003dfbfc7f42c9a0a0ccce83f6a3ada167064597
Artem <lebro53@mail.ru>
2023-04-15 21:02:41 +0300

    Add solution task3

--- DirtyPython/task3.py
+++ DirtyPython/task3.py
@@ -1,1 +1,23 @@
-ведро
+# 3. ИСТИННОСТЬ ПРЕДИКАТ
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
+# Данное выражение истинно при любых значениях предикат
+# (предикат - переменная, которая может иметь только два значения True или False)
+# Напишите программу, которая докажет это.
+
+x = int(input('Enter value x = 1 or 0: '))
+y = int(input('Enter value y = 1 or 0: '))
+z = int(input('Enter value z = 1 or 0: '))
+
+if (x == 1):
+    x = True
+else:
+    x = False
+if (y == 1):
+    x = True
+else:
+    x = False
+if (z == 1):
+    x = True
+else:
+    x = False
+print(not (x * y * z) == (not x) + (not y) + (not z))
--- DirtyPython/task5.py
+++ DirtyPython/task5.py
@@ -6,13 +6,16 @@
 
 number = int(input('Enter number: '))
 i = 1
-while (i <= number):
-    if (i % 3 == 0 and i % 5 == 0):
-        print('FizzBuzz')
-    elif (i % 3 == 0):
-        print('Fizz')
-    elif (i % 5 == 0):
-        print('Buzz')
-    else:
-        print(i)
-    i += 1
+if number < 1:
+    print('Enter a positive natural number')
+else:
+    while (i <= number):
+        if (i % 3 == 0 and i % 5 == 0):
+            print('FizzBuzz')
+        elif (i % 3 == 0):
+            print('Fizz')
+        elif (i % 5 == 0):
+            print('Buzz')
+        else:
+            print(i)
+        i += 1
