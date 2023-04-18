0911d37f41f74ff9bde2f16c96988e6049328c68
Artem <lebro53@mail.ru>
2023-04-18 20:32:31 +0300

    Add solution task3 v.2.0

--- DirtyPython/Lection3. Homework/Task3.py
+++ DirtyPython/Lection3. Homework/Task3.py
@@ -4,61 +4,36 @@
 # координатам(например B7 = Белая), если точно знаем, что клетка А1 - черная
 letter_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 numbers_list = ['1', '2', '3', '4', '5', '6', '7', '8']
-board_list = []
 black_list = []
 white_list = []
 search = input('Enter the desired cell: ').upper()
-flag = True
-for item_letter in letter_list:
-    for item_numbers in numbers_list:
-        board_list.append(item_letter + item_numbers)
+black_flag = 0
+white_flag = 0
 
-for i in range(len(board_list) - 56):
-    if i % 2 == 0:
-        black_list.append(board_list[i])
-    else:
-        white_list.append(board_list[i])
-
-for i in range(8, len(board_list) - 48):
-    if i % 2 != 0:
-        black_list.append(board_list[i])
-    else:
-        white_list.append(board_list[i])
-for i in range(16, len(board_list) - 40):
-    if i % 2 == 0:
-        black_list.append(board_list[i])
-    else:
-        white_list.append(board_list[i])
-for i in range(24, len(board_list) - 32):
-    if i % 2 != 0:
-        black_list.append(board_list[i])
-    else:
-        white_list.append(board_list[i])
-for i in range(32, len(board_list) -24):
-    if i % 2 == 0:
-        black_list.append(board_list[i])
-    else:
-        white_list.append(board_list[i])
-for i in range(40, len(board_list) - 16):
-    if i % 2 != 0:
-        black_list.append(board_list[i])
-    else:
-        white_list.append(board_list[i])
-for i in range(48, len(board_list) - 8):
-    if i % 2 == 0:
-        black_list.append(board_list[i])
-    else:
-        white_list.append(board_list[i])
-for i in range(56, len(board_list) ):
-    if i % 2 != 0:
-        black_list.append(board_list[i])
-    else:
-        white_list.append(board_list[i])
+for i in range(len(letter_list)):
+    for j in range(len(numbers_list)):
+        if i % 2 != 0:
+            if j % 2 != 0:
+                black_list.append(letter_list[i] + numbers_list[j])
+            else:
+                white_list.append(letter_list[i] + numbers_list[j])
+        else:
+            if j % 2 == 0:
+                black_list.append(letter_list[i] + numbers_list[j])
+            else:
+                white_list.append(letter_list[i] + numbers_list[j])
 
 for i in black_list:
     if search == i:
-        flag = False
-if flag == False:
-    print('Black')
+        black_flag = 1
+
+for i in white_list:
+    if search == i:
+        white_flag = 1
+
+if black_flag == 1:
+    print(f'{search} = Black')
+elif white_flag == 1:
+    print(f'{search} = White')
 else:
-    print('White')
+    print('Not found')
