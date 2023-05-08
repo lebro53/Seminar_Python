f14e89294cd018ac408f8ccaaee62d149f0b8e0a
Artem <lebro53@mail.ru>
2023-05-08 09:26:58 +0300

    Add complition task1 and add task2

--- DirtyPython/Lection4. Part1. Homework/Task1.py
+++ DirtyPython/Lection4. Part1. Homework/Task1.py
@@ -4,4 +4,32 @@
 #   - сравнить длину строк из цифр и из букв и вернуть ту, которая длиннее
 
 
-def
+def get_letters(text: str) -> str:
+    text_letter = ''
+    for elem in range(len(text)):
+        if not text[elem].isdigit():
+            text_letter += text[elem]
+    return text_letter
+
+
+def get_numbers(text: str) -> str:
+    text_num = ''
+    for elem in range(len(text)):
+        if text[elem].isdigit():
+            text_num += text[elem]
+    return text_num
+
+def comparsion_len_num_and_letter(num_text: str, letter_text: str) -> None:
+    if num_text > letter_text:
+        print(f'Строка состоящая из цифр {num_text} больше')
+    elif num_text == letter_text:
+        print(f'Строка состоящая из цифр {num_text} равна строке из букв {letter_text}')
+    else:
+        print(f'Строка состоящая из букв {letter_text} больше')
+
+
+letter = get_letters('qwerty12345')
+print(letter)
+number = get_numbers('qwerty12345')
+print(number)
+comparsion_len_num_and_letter(number, letter)
