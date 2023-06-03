8d584f2c1123a2463134446c431ceea36ae2b184
Artem <lebro53@mail.ru>
2023-06-03 14:25:31 +0300

    model PB

--- PB_Module/controller.py
+++ PB_Module/controller.py
@@ -2,10 +2,11 @@ import view
 import model
 import text
 
-copy_open_file = model.open_file()
+my_PB = model.PhoneBook()
+copy_open_file = my_PB.open_file()
 def link_new_PB_copy(copy_file):
     view.text_print(text.pb_link)
-    model.print_phone_book(copy_file)
+    my_PB.print_phone_book(copy_file)
 
 def start(copy_open_PB: list):
     while True:
@@ -13,22 +14,22 @@ def start(copy_open_PB: list):
 
         match choice:
             case 1:
-                model.print_phone_book(copy_open_PB)
+                my_PB.print_phone_book(copy_open_PB)
             case 2:
-                copy_open_PB = model.add_contact(copy_open_PB)
+                copy_open_PB = my_PB.add_contact(copy_open_PB)
                 view.text_print(text.successful)
                 link_new_PB_copy(copy_open_PB)
             case 3:
-                found = model.search_contact(copy_open_PB)
+                found = my_PB.search_contact(copy_open_PB)
                 view.f_string_print(text.search_link, found)
             case 4:
-                copy_open_PB = model.change_contact(copy_open_PB)
+                copy_open_PB = my_PB.change_contact(copy_open_PB)
                 link_new_PB_copy(copy_open_PB)
             case 5:
-                copy_open_PB = model.delete_contact(copy_open_PB)
+                copy_open_PB = my_PB.delete_contact(copy_open_PB)
                 view.text_print(text.successful)
                 link_new_PB_copy(copy_open_PB)
             case 6:
-                view.save_file_func(model.save_file, copy_open_PB)
+                view.save_file_func(my_PB.save_file, copy_open_PB)
             case 7:
                 break
